6cfdd6d24b4c306f975e316cde0a3e715a99402a
Marc Hurst <marcrhurst@gmail.com>
2022-06-20 15:44:52 -0400

    Moved MongoClient to external file

--- db.js
+++ db.js
@@ -1,4 +1,18 @@
-const {MongoClient} = require('mongodb')
+const MongoClient = require('mongodb').MongoClient
 const connectionStr = process.env.DBSTRING
+const dbName = "muskokagrown"
 
-const client = new MongoClient(connectionStr)
+// const client = new MongoClient(connectionStr)
+
+const getDB = _ => {
+    MongoClient.connect(connectionStr, {useUnifiedTopology: true})
+        .then(client => {
+            console.log(`Connected to ${dbName} on MongoDB`)
+            return client.db(dbName)
+        })
+        .catch(err => console.error(`Error connecting to MongoDB: ${err}`))
+}
+    
+module.exports = {
+    getDB
+}

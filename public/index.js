d993fced0c15cd7fa46e57c693212cb38d9e0195
Marc Hurst <marcrhurst@gmail.com>
2022-06-22 09:31:21 -0400

    cors tags on the fetch.

--- public/index.js
+++ public/index.js
@@ -3,11 +3,18 @@ document.addEventListener("DOMContentLoaded", _ => {
         console.log("Clicked!")
         console.log(isInputValid())
         if (isInputValid()) {
-            let response = await fetch(`https://muskoka-grown-coa.herokuapp.com/api/${document.querySelector("#lotNumInput").value}`)
+            let response = await fetch(`https://muskoka-grown-coa.herokuapp.com/api/${document.querySelector("#lotNumInput").value}`, {
+                mode: 'no-cors',
+                method: "GET",
+                
+                // headers: {
+                //     "Content-Type": "application/json"
+                // }
+            })
             let data = await response.json()
             console.log(data)
         } else {
-
+            document.querySelector("#errorPara").innerText = `Please ensure that your 7 digit lot # is fully included.`
         }
     })
 })
